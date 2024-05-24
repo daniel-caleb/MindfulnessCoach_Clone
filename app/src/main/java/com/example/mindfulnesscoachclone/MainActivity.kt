d660c1b386538573f13c7226497cd8d9367fac57
Clair <clareajara@gmail.com>
2024-05-24 08:07:01 +0300

    Modifications

--- app/src/main/java/com/example/mindfulnesscoachclone/MainActivity.kt
+++ app/src/main/java/com/example/mindfulnesscoachclone/MainActivity.kt
@@ -3,44 +3,47 @@ package com.example.mindfulnesscoachclone
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.mindfulnesscoachclone.ui.theme.MindfulnessCoachCloneTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MindfulnessCoachCloneTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+
+            HomePage()
+
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun HomePage(){
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MindfulnessCoachCloneTheme {
-        Greeting("Android")
+    Row (
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalArrangement = Arrangement.Center
+    ){
+        Text(text = "Mindfulness Coach")
     }
+
 }
